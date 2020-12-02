53c4c0139aaa723bf73968dddc7f4a5b566bab20
Manik <emanikchannel10@gmail.com>
2020-12-02 07:33:55 +0530

    Update

--- Cog/responses/main.py
+++ Cog/responses/main.py
@@ -38,21 +38,21 @@ class Angry(commands.Cog, name="Response Commands"):
         # Prepare for what you are about to read, I call it being hella sexy!!!
 
         triggers = {
-            "gamers": ["gamer", "game", "play", "playing"],
+            "gamers": ["gamer", "game", "play", "playing", "Demo"],
             "pingers": ['ping', 'mention', 'pinged', 'mentioned', 'everyone'],
             "cursed": ['2020', 'twenty twenty', 'worst year ever', 'worst year'],
-            "cringed": ['minions', 'facebook', 'LOL!'],
-            "sexy": ['sex', 'penis', 'dick', 'cock', 'cbt', 'cum', 'femboy', 'pp', 'hentai', 'felix', 'gay', 'bussy'],
-            "ews": ['boobs', 'tits', 'pussy', 'boob']
+            "cringed": ['funny', 'facebook', 'LOL!', 'joke'],
+            "sexy": ['sex', 'penis', 'dick', 'cock', 'cbt', 'cum', 'femboy', 'pp', 'hentai', 'felix', 'gay', 'bussy', gandu],
+            "ews": ['boobs', 'tits', 'pussy', 'boob', 'nigga', 'chodu']
         }
 
         responses = {
             "gamers": ['gamer gunk', 'Download ram here <https://julians.work/angry>', 'Can I join?', 'Is the party full?', 'Whats the score?', 'Did you start without me? Guys not again...', 'I have no one to play with. Every time I try to play a game I get kicked. Why does this keep happening to me? Why...'],
-            "pingers": ['Who did it? I\'ll ping them back', 'I don\'t like this', 'All I can see is pings', 'Pong!!111!11!', 'Guys you can turn of pings in the notification settings for this server!', 'Please no not another ping....'],
+            "pingers": ['kisne kara be? I\'mai dekhtaa hoo, usko waapas ping karungaa', 'Mereko nahi pasand ye Ch%$#@*p', 'pings karo bha bhar ke', 'haa bolnaa', 'Guys you can turn of pings in the notification settings for this server! theek karo jaake', 'ping mat karo saalo....'],
             "cursed": ['Please don\'t curse us anymore', 'No, not 2020. Please no', 'I heard the aliens are up next', 'Whats 2020?', 'Oh god, what have you done?'],
-            "cringed": ['MY KIDS LOVE MINIONS AS WELL!!!!', 'Are you also anti-vax? I don\' want my son to get autism!', '#flatEarthSociety', 'Trump 2020'],
+            "cringed": ['Achaaaa.. ha ha', 'Are you also anti-vax? I don\' want my son to get autism!', '#flatEarthSociety', 'Trump 2020'],
             "sexy": ['hot', 'mmmmmm', 'tasty', 'Yo, can I get sum?', 'Give me... now!', 'I do love some of that', 'I\'m gonna cum', 'Yes please!', 'I\'ll have what they\'re having', 'How much?', 'Is there any left?', 'tickling the pickle', 'I\'m starting to become horny not angry'],
-            "ews": ['ewwww', 'ucky', 'me no likey', 'I\'m gonna be sick', 'Why would you say that?', 'cock is better btw']
+            "ews": ['ewwww', 'ucky', 'me no likey', 'Chheee badtameez', 'Why would you say that?', 'cock is better btw']
         }
 
         for word in message.content.split(" "):
